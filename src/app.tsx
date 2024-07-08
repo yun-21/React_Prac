d72a8023448b12a1fa0288ca96c7ed7f596caa72
shinjiyun <ranji12@naver.com>
2024-07-08 12:41:35 +0900

    react로 작성

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+const App:React.FC = () =>{
+  return (
+    <div>
+      <h1>Hello,World</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+  );
